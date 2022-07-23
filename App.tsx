5d28246599f4bf17dd4eb9858900e82433fabead
ermekov-davlet-u <d.ermekovv@gmail.com>
2022-07-23 14:27:53 +0600

    start Project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,106 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ChangeEvent, useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, TouchableHighlight, View, ViewStyle, Linking } from 'react-native';
+
+
+interface PostType{
+  completed: boolean
+  id: number
+  title: string
+  userId: number
+}
 
 export default function App() {
+  const [ text, setText] = useState<string>("")
+  const [ posts, setPosts ] = useState<PostType[]>([])
+  const [ textInp, setTextInp ] = useState<{
+    
+    backgroundColor: string
+  }>({
+    backgroundColor: '#fff'
+  })
+
+  function onFocus() {
+    setTextInp({
+        backgroundColor: 'green'
+    })
+  }
+
+  function onBlur() {
+    setTextInp({
+      backgroundColor: '#ededed'
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View>
+        <TextInput style={{
+          width: "290px",
+          height: "24 px",
+          margin: "48px",
+          padding: 9,
+          borderColor: 'gray',
+          borderWidth: 1,
+          ...textInp
+          }} 
+          editable
+          multiline
+          value={text}
+          onChangeText={(text) => {
+            setText(text);
+          }}
+          onFocus={onFocus}
+          onBlur={onBlur}
+        defaultValue="Привет!"/>
+      </View>
+      <View>
+        {
+          posts.map(item => {
+            return(
+              <View>
+                <Text 
+                style={{
+                  borderColor: "#fff",
+                  borderWidth: 2,
+                  maxWidth: 230,
+                  margin: 4,
+                  padding: 8
+                }}
+              >
+                {
+                  item.title
+                }
+              </Text>
+              {/* <TouchableHighlight onPress={() => {
+                Linking.canOpenURL("https://reactnative.dev/docs/linking")
+              }}>Learn more...</TouchableHighlight> */}
+              </View>
+            )
+          })
+        }
+      </View>
+      <View>
+        <Button
+          onPress={async() => {
+            const a = await fetch('https://jsonplaceholder.typicode.com/todos')
+              .then(response => response.json())
+              setPosts(a);
+          }}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Button
+          onPress={async() => {
+              console.log(posts);
+          }}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +108,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
 });
